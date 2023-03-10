82f1f8d5f255c4754a4c316c773f2e7955781f7b
Jngvarr <jngvarr@gmail.com>
2023-03-10 16:50:13 +0700

    homework8 ready

--- sem8/homework/src/Main.java
+++ sem8/homework/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Building building = new Building(100, 40);
+        Building building = new Building(100, 23);
         Creature creature = new Creature(111, 70, 150, 101);
-        Creature creature2 = new Creature(111, 25, 150, 30);
+        Creature creature2 = new Creature(111, 25, 150, 40);
         Render render = new Render();
         render.render(building);
         render.render(creature);
