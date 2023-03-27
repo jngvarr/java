85ac80d7af679b71d0960af8d8f344c89612ead1
jngvarr <jngvarr@gmail.com>
2023-03-27 17:32:19 +0700

    javarush add

---
+++ codewar/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    //        solution('abc', 'bc') // returns true
+//        solution('abc', 'd') // returns false);
+    public static void main(String[] args) {
+        System.out.println( "abc".endsWith("bc"));
+        System.out.println( "abc".endsWith("d"));
+
+
+
+    }
+}
