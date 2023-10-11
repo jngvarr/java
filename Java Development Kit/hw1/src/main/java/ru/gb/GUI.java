8ddd1477fde4152abeb6da6bd26822baba644507
jngvarr <jngvarr@gmail.com>
2023-10-11 17:14:00 +0700

    hw1 done!

--- Java Development Kit/hw1/src/main/java/ru/gb/GUI.java
+++ Java Development Kit/hw1/src/main/java/ru/gb/GUI.java
@@ -2,8 +2,12 @@ package ru.gb;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class GUI extends JFrame {
     public static final int WINDOW_HEIGHT = 200;
@@ -18,46 +22,73 @@ public class GUI extends JFrame {
     JTextArea jTextAreaMessages;
     JTextField jTextFieldLogin;
     JTextField jTextFieldMessage;
-    JList jList;
+    //JList jList;
 
     public GUI(Server server) {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Chat Client");
         setLocationRelativeTo(server);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         add(createBottomPanel(), BorderLayout.SOUTH);
         add(createUpPanel(), BorderLayout.NORTH);
         jTextAreaMessages = new JTextArea();
-        jList = new JList<>();
+        //jList = new JList<>();
         add(jTextAreaMessages);
         //jList.setListData();
+        String qq = this.jTextFieldLogin.getText();
+        System.out.println(qq);
+        //setFocusable(true);
+        Logger logger = new Logger();
+        jTextFieldMessage.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+                    jButtonSend.doClick();
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
 
         jButtonSend.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 server.jTextArea.append(jTextFieldMessage.getText() + "\n");
+                String result = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + ": " +
+                        jTextFieldLogin.getText() + ": " + jTextFieldMessage.getText() + "\n";
                 System.out.println(jTextFieldMessage.getText());
+                jTextAreaMessages.append(result);
+                logger.writeLogToFile(result);
                 jTextFieldMessage.setText("");
-                System.out.println(jTextFieldLogin.getText() + " 3");
             }
         });
         jButtonLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 jPanelLoginData.setVisible(false);
-                server.setTitle(server.getTitle() + " (" + /*jTextFieldLogin.getText() +*/ ")");
-                System.out.println(jTextFieldLogin.getText() + " 1");
+                server.setVisible(false);
+                setTitle(getTitle() + " (" + jTextFieldLogin.getText() + ")");
+                // jPanelMessages.setFocusable(true);
+                jTextAreaMessages.setText(String.valueOf(logger.readLogTFromFile()));
             }
         });
     }
 
     private Component createUpPanel() {
         jPanelLoginData = new JPanel(new GridLayout(2, 3));
-        JTextField jTextFieldLogin = new JTextField("Фёдор Михалыч");
+        jTextFieldLogin = new JTextField("Фёдор Михалыч");
         JPasswordField jPasswordField = new JPasswordField("password");
         JTextField jTextFieldIP = new JTextField("192.168.0.1");
         JTextField jTextFieldPort = new JTextField("8080");
         jButtonLogin = new JButton("Login");
-        System.out.println(jTextFieldLogin.getText() + " 2");
         jPanelLoginData.add(jTextFieldIP);
         jPanelLoginData.add(jTextFieldPort);
         jPanelLoginData.add(jTextFieldLogin);
