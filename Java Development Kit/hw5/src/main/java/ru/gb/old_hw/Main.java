9f60d1a4e7826801c9755670a26bcdc19d0c0bcc
jngvarr <jngvarr@gmail.com>
2023-11-08 15:52:26 +0700

    hw5 done

--- Java Development Kit/hw5/src/main/java/ru/gb/old_hw/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.old_hw;
-
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Table table = new Table();
-        table.allPhilosopherTryToEat();
-    }
-}
