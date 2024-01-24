74ac59e6870fa17464c4abfcbac201f546b9c535
jngvarr <jngvarr@gmail.com>
2024-01-24 17:02:18 +0700

    lect3 in progress

---
+++ Spring/lecture3/myWebApplication/myWebApplication/src/main/java/ru/gb/myWebApplication/Task.java
@@ -0,0 +1,65 @@
+package ru.gb.myWebApplication;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+public class Task {
+    public enum Status {
+        TO_DO,
+        IN_PROGRESS,
+        DONE
+    }
+
+    private UUID id;
+    private String name;
+    private String description;
+    private Status status;
+    private LocalDateTime competitionTime;
+
+    public Task(String name, String description) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.description = description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCompetitionTime() {
+        return competitionTime;
+    }
+
+    public void setCompetitionTime(LocalDateTime competitionTime) {
+        this.competitionTime = competitionTime;
+    }
+}
---
+++ Spring/lecture3/myWebApplication/myWebApplication/src/main/java/ru/gb/myWebApplication/TaskController.java
@@ -0,0 +1,23 @@
+package ru.gb.myWebApplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+}
