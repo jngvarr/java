a0ab71d0e4b2d15a7aff9b7403f1a245fc33b3f6
jngvarr <jngvarr@gmail.com>
2024-01-30 16:58:33 +0700

    lec5 added

--- Spring/lecture5/src/main/java/com/example/demo/User.java
+++ Spring/lecture5/src/main/java/com/example/demo/User.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
---
+++ Spring/lecture5/src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> getAllUsers() {
+        repository.save(new User(null, "igor", "qq@.qq.ru"));
+        repository.save(new User(null, "jack", "jk@.qq.ru"));
+        repository.save(new User(null, "john", "jn@.qq.ru"));
+        return repository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return repository.findById(id).get();
+    }
+}
