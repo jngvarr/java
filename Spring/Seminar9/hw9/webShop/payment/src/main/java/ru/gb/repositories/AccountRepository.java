0fa03bf3044267236f8a3e2cf4c67d95f2c460a4
jngvarr <jngvarr@gmail.com>
2024-02-20 15:43:12 +0700

    hw9 done

--- Spring/Seminar9/hw9/webShop/payment/src/main/java/ru/gb/repositories/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.gb.model.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
