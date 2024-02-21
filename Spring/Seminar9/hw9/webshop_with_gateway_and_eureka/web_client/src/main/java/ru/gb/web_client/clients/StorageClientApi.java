245d2dffd1f3959b9b4be1ecabd4ad01402ac53b
Jngvarr <jngvarr@inbox.ru>
2024-02-21 17:54:41 +0700

    hw9 done

--- Spring/Seminar9/hw9/webshop_with_gateway_and_eureka/web_client/src/main/java/ru/gb/web_client/clients/StorageClientApi.java
+++ Spring/Seminar9/hw9/webshop_with_gateway_and_eureka/web_client/src/main/java/ru/gb/web_client/clients/StorageClientApi.java
@@ -1,8 +1,27 @@
 package ru.gb.web_client.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.gb.web_client.model.Order;
+import ru.gb.web_client.model.Product;
 
-@FeignClient(name = storage)
+import java.util.List;
+
+@FeignClient(name = "storage")
 public interface StorageClientApi {
+    @GetMapping
+    List<Product> getProducts();
 
+    @PostMapping("/{id}/reserve")
+    ResponseEntity<?> reserveProduct(@PathVariable Long id, @RequestBody Order order);
+
+    @PostMapping("/{id}/reserve/rollback")
+    void rollbackReserve(@PathVariable Long id, @RequestBody Order order);
+
+    @PostMapping("/{id}")
+    ResponseEntity<?> bay(@PathVariable Long id, @RequestBody Order order);
 }
