ed24ecb58bcaddc9baaa857ad4462cb4e011c9f7
Jngvarr <jngvarr@inbox.ru>
2024-02-21 18:02:27 +0700

    hw9 done

--- Spring/Seminar9/hw9/web-shop-with-api-gateaway/storage/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE if not exists products (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
-    description VARCHAR(50) NOT NULL,
-    price DOUBLE NOT NULL check (price > 0 ),
-    quantity LONG NOT NULL check (quantity >= -1)
-);
