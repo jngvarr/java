4df4f4a004a605b39b59d2751db283fbb8c79adf
jngvarr <jngvarr@gmail.com>
2024-02-26 17:09:19 +0700

    sem12 added

---
+++ Spring/Seminar12/qq/sem12_example1/src/main/java/ru/gb/sem12_example1/Sem12Example1Application.java
@@ -0,0 +1,13 @@
+package ru.gb.sem12_example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem12Example1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem12Example1Application.class, args);
+    }
+
+}
---
+++ Spring/Seminar12/qq/sem12_example2/src/main/java/ru/gb/sem12_example2/Sem12Example2Application.java
@@ -0,0 +1,13 @@
+package ru.gb.sem12_example2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem12Example2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem12Example2Application.class, args);
+	}
+
+}
