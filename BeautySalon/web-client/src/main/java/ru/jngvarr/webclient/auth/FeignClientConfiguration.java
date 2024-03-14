3f43817dcc4e2293053c0f95c8adb2c99769f96f
jngvarr <jngvarr@gmail.com>
2024-03-14 16:50:01 +0700

    salon in progress 1.1.7

---
+++ BeautySalon/web-client/src/main/java/ru/jngvarr/webclient/auth/FeignClientConfiguration.java
@@ -0,0 +1,16 @@
+package ru.jngvarr.webclient.auth;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignClientConfiguration {
+    @Value("${your.token}") // Подставьте имя свойства, в котором хранится ваш токен
+    private String token;
+
+    @Bean
+    public TokenInterceptor tokenInterceptor() {
+        return new TokenInterceptor(token);
+    }
+}
