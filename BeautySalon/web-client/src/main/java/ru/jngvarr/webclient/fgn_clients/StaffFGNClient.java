e43095483cd17d32074763c5c75d1eb9417e5e6f
jngvarr <jngvarr@gmail.com>
2024-03-18 22:41:48 +0700

    some commit

--- BeautySalon/web-client/src/main/java/ru/jngvarr/webclient/fgn_clients/StaffFGNClient.java
+++ BeautySalon/web-client/src/main/java/ru/jngvarr/webclient/fgn_clients/StaffFGNClient.java
@@ -3,5 +3,5 @@ package ru.jngvarr.webclient.fgn_clients;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(name = "staff")
-public interface staffFGNClient {
+public interface StaffFGNClient {
 }
