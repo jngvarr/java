79e8edeebbf724d0e3fd42463e8c29328b760b5e
jngvarr <jngvarr@gmail.com>
2024-05-03 16:28:53 +0700

    salon in progress 1.2.28

--- BeautySalon/appt-management/src/main/java/ru/jngvarr/appointmentmanagement/AppointmentManagementApplication.java
+++ BeautySalon/appt-management/src/main/java/ru/jngvarr/appointmentmanagement/AppointmentManagementApplication.java
@@ -1,8 +1,14 @@
 package ru.jngvarr.appointmentmanagement;
 
+import feign_clients.ClientFeignClient;
+import feign_clients.ServiceFeignClient;
+import feign_clients.StaffFeignClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
 @EnableFeignClients(basePackages = {"feign_clients"})
@@ -12,5 +18,4 @@ public class AppointmentManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppointmentManagementApplication.class, args);
     }
-
 }
---
+++ BeautySalon/appt-management/src/main/java/ru/jngvarr/appointmentmanagement/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package ru.jngvarr.appointmentmanagement.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//@EnableFeignClients
+//public class MyFeignConfig {
+//
+//    @LoadBalanced
+//    @Bean
+//    public ServiceFeignClient serviceFeignClient() {
+//        return new ServiceFeignClient(); // Ваш Feign клиент
+//    }
+//}
