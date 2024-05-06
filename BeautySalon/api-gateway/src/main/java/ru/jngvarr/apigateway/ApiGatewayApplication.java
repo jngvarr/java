be9c9b7392bdf9e8d7f7425b6ac11d335b51e52a
jngvarr <jngvarr@gmail.com>
2024-05-06 09:14:45 +0700

    salon in progress 1.2.30

--- BeautySalon/api-gateway/src/main/java/ru/jngvarr/apigateway/ApiGatewayApplication.java
+++ BeautySalon/api-gateway/src/main/java/ru/jngvarr/apigateway/ApiGatewayApplication.java
@@ -12,12 +12,21 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("storage", r -> r.path("/storage/**")
+                .route("clients", r -> r.path("/clients/**")
                         .uri("http://localhost:8081/"))
-                .route("payment", r -> r.path("/payment/**")
-                        .uri("http://localhost:8082/")).build();
+                .route("services", r -> r.path("/services/**")
+                        .uri("http://localhost:8082/"))
+                .route("storage", r -> r.path("/storage/**")
+                        .uri("http://localhost:8083/"))
+                .route("storage", r -> r.path("/staff/**")
+                        .uri("http://localhost:8084/"))
+                .route("appointment", r -> r.path("/visits/**")
+                        .uri("http://localhost:8085/"))
+                .route("eserver", r -> r.path("/eureka/**")
+                        .uri("http://localhost:8761/")).build();
     }
 }
