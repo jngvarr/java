dd5e0b62cbd16429348788c10b7cd286f48b38f2
jngvarr <jngvarr@gmail.com>
2024-06-07 00:20:00 +0700

    ng 2.0.17

--- BeautySalon/angularclient/src/app/model/interceptors/auth.interceptor.ts
+++ BeautySalon/angularclient/src/app/model/interceptors/auth.interceptor.ts
@@ -1,29 +1,34 @@
-
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, switchMap, tap } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, switchMap, tap} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {Router} from "@angular/router";
 
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   private isRefreshing = false;
 
-  constructor(private http: HttpClient) {}
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = sessionStorage.getItem('token');
-    let authRequest = request;
-
+  constructor(private http: HttpClient, private router : Router) {
+  }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = sessionStorage.getItem("token")
+    let authRequest = req;
     if (token) {
-      authRequest = this.addToken(request, token);
+      authRequest = this.addToken(req, token);
     }
 
     return next.handle(authRequest).pipe(
       catchError(error => {
         if (error instanceof HttpErrorResponse && error.status === 401) {
-          return this.handle401Error(authRequest, next);
+          // Перенаправляем пользователя на сервер для повторной аутентификации
+          //TODO реализовать обновление accessToken
+          this.router.navigate(['/login'], {
+            queryParams: {
+              returnUrl: req.url
+            }
+          });
         }
         return throwError(error);
       })
@@ -31,34 +36,12 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   private addToken(req: HttpRequest<any>, token: string): HttpRequest<any> {
-    return req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`),
-      // Добавляем заголовок Content-Type: application/json
-      setHeaders: {
-        'Content-Type': 'application/json'
-      }
+    const clonedRequest = req.clone({
+      headers: req.headers
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json')
     });
-  }
-
-  private handle401Error(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (!this.isRefreshing) {
-      this.isRefreshing = true;
-      return this.http.post<any>('/users/refresh', { token: sessionStorage.getItem('refreshToken') })
-        .pipe(
-          catchError(error => {
-            this.isRefreshing = false;
-            return throwError(error);
-          }),
-          catchError(() => {
-            this.isRefreshing = false;
-            return throwError('Refresh token failed');
-          }),
-          catchError(() => {
-            return next.handle(req);
-          })
-        );
-    } else {
-      return next.handle(req);
-    }
+    console.log('Cloned request headers:', clonedRequest.headers.keys());
+    return clonedRequest;
   }
 }
