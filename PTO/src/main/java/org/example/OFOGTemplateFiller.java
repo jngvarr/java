9d59ec98d429bcaeeee9c31ed2aff1b610dff210
jngvarr <jngvarr@gmail.com>
2024-11-27 02:08:48 +0700

    27.11.2024

--- PTO/src/main/java/org/example/OFOGTemplateFiller.java
+++ PTO/src/main/java/org/example/OFOGTemplateFiller.java
@@ -13,34 +13,36 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class OFOGTemplateFiller {
     private static final DateTimeFormatter DATE_FORMATTER_DDMMYYYY = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
     private static final Logger logger = LoggerFactory.getLogger(OFOGTemplateFiller.class);
+    private static int iReportRows;
 
     public static void main(String[] args) throws IOException {
         // Путь к вашему шаблону
         String templatePath = "d:\\Downloads\\пто\\month_reports\\templates\\month_report_template.xlsx";
-//        String dataPath = "c:\\Users\\admin\\YandexDiskUKSTS\\YandexDisk\\ПТО РРЭ РЖД\\План ОТО\\ОЖ 2024.xlsx";
-        String dataPath = "d:\\YandexDisk\\ПТО РРЭ РЖД\\План ОТО\\ОЖ 2024.xlsx";
+        String dataPath = "c:\\Users\\admin\\YandexDiskUKSTS\\YandexDisk\\ПТО РРЭ РЖД\\План ОТО\\ОЖ 2024.xlsx";
+//        String dataPath = "d:\\YandexDisk\\ПТО РРЭ РЖД\\План ОТО\\ОЖ 2024.xlsx";
         // Путь для сохранения нового файла
         String outputFilePathOFlog = "d:\\Downloads\\пто\\month_reports\\filled_operational_failure_log.xlsx";
         String outputFilePathOfogIr = "d:\\Downloads\\filled_inspection_report.xlsx";
         int dateColumnIndex = 16;
         int targetMonth = LocalDate.now().getMonthValue();
         int faultReasonColNum = 18;
-        int targetLogSheetColNum = 0;
-        int targetIReportColNum = 0;
+        int targetLogSheetRowNum = 0;
+        int targetIReportRowNum = 0;
         int reportRows = 0;
-
+        int ofLogSheetInsertPosition = 8;
+        int iReportSheetInsertPosition = 9;
 
         // Открываем файл-шаблон
         try (FileInputStream templateFis = new FileInputStream(templatePath);
@@ -77,15 +79,23 @@ public class OFOGTemplateFiller {
                         if (month == eventMonth && year == eventYear) {
                             logger.info("Содержание строки №{}, {}", row.getRowNum(), faultReasonCell.getStringCellValue());
                             reportRows++;
-                            Row targetRow;
                             // Копируем строку в целевой лист
-                            if (faultReasonCell != null && faultReasonCell.getCellType() == CellType.STRING &&
-                                    !faultReasonCell.getStringCellValue().equals("Уточнение реквизитов ТУ (подана заявка на корректировку НСИ")) {
-                                targetRow = ofLogSheet.createRow(targetLogSheetColNum++ + 8);
-                            } else {
-                                targetRow = iReportSheet.createRow(targetIReportColNum++ + 9);
+                            if (faultReasonCell.getCellType() == CellType.STRING && !faultReasonCell.getStringCellValue().trim().isEmpty()) {
+
+                                String faultReason = faultReasonCell.getStringCellValue().trim()
+                                        .replace("\u00A0", "")
+                                        .replaceAll("\\s+", " "); // Убираем неразрывные пробелы и лишние пробелы
+
+                                logger.info("Обрабатываемая строка: '{}'", faultReason);
+
+                                if (!faultReason.equals("Уточнение реквизитов ТУ (подана заявка на корректировку НСИ)")) {
+                                    copyRowsData(row, ofLogSheet, ofLogSheetInsertPosition++, commonCellStyle, true);
+                                } else {
+                                    copyRowsData(row, iReportSheet, iReportSheetInsertPosition++, commonCellStyle, false);
+                                }
                             }
-                            copyRowsData(row, targetRow, commonCellStyle);
+
+
                         }
                     }
 
@@ -99,7 +109,7 @@ public class OFOGTemplateFiller {
                 try (FileOutputStream fos2 = new FileOutputStream(outputFilePathOfogIr)) {
                     templateWorkbook.write(fos2);
                 }
-
+                templateWorkbook.close(); // Закрываем Workbook
                 System.out.println("Данные успешно добавлены и сохранены в файл: " + outputFilePathOFlog);
 
             } catch (IOException e) {
@@ -118,66 +128,93 @@ public class OFOGTemplateFiller {
         logger.info("Совпавших строк в отчете {}:", reportRows);
     }
 
-    private static void copyRowsData(Row sourceRow, Row targetRow, CellStyle style) {
-        String[] dates = new String[12];
-        Cell cell = (sourceRow.getCell(16));
+    private static void copyRowsData(Row sourceRow, XSSFSheet sheet, int insertPosition, CellStyle style, boolean ofog) {
+        String[] dates;
+        if (ofog) {
+            dates = prepareOfogData(sourceRow);
+        } else dates = prepareIRepData(sourceRow);
 
+        if (insertPosition <= sheet.getLastRowNum()) {
+            sheet.shiftRows(insertPosition, sheet.getLastRowNum(), 1);
+        }
+
+        Row targetRow = sheet.createRow(insertPosition);
+        writeRowData(targetRow, dates, style);
+    }
+
+    private static String[] prepareIRepData(Row sourceRow) {
+
+        String equipment;
+        String equipmentNum;
+        String equipmentType;
+        String row = getCellStringValue(sourceRow.getCell(13));
+        if (row.isEmpty()) {
+            equipment = "ИВЭК";
+            equipmentNum = getCellStringValue(sourceRow.getCell(14));
+            equipmentType = "DC-1000";
+        } else {
+            equipmentNum = getCellStringValue(sourceRow.getCell(13));
+            equipmentType = getCellStringValue(sourceRow.getCell(12));
+            equipment = "ИИК";
+        }
+        return new String[]{
+                String.valueOf(iReportRows++),
+                equipment,
+                equipmentType,
+                equipmentNum,
+                getCellStringValue(sourceRow.getCell(7)),
+                getCellStringValue(sourceRow.getCell(9)),
+                getCellStringValue(sourceRow.getCell(4)),
+                getCellStringValue(sourceRow.getCell(5)),
+                getCellStringValue(sourceRow.getCell(21)),
+        };
+    }
+
+    private static String[] prepareOfogData(Row sourceRow) {
+        Cell cell = sourceRow.getCell(16);
         LocalDate restorationDate = cell.getLocalDateTimeCellValue().toLocalDate();
         LocalDate failureDetectionDate = subtractRandomDays(restorationDate);
 
-        dates[0] = failureDetectionDate.format(DATE_FORMATTER_DDMMYYYY);
-        dates[1] = generateRandomTime(9, 17);
-        dates[2] = failureDetectionDate.minusDays(1).format(DATE_FORMATTER_DDMMYYYY) + " "
-                + generateRandomTime(0, 23);
-        dates[3] = "ЗСЖД";
-        dates[4] = cell.getRow().getCell(7).getStringCellValue();
-        dates[5] = concatAndCopyRowsData(cell.getRow());
-        dates[6] = cell.getRow().getCell(17).getStringCellValue();
-        dates[7] = "Демянчук В.М., диспетчер";
-        dates[8] = restorationDate.format(DATE_FORMATTER_DDMMYYYY);
-        dates[9] = generateRandomTime(9, 17);
-        dates[10] = cell.getRow().getCell(18).getStringCellValue();
-        dates[11] = cell.getRow().getCell(20).getStringCellValue() + ", инженер ООО УК СТС";
+        return new String[]{
+                failureDetectionDate.format(DATE_FORMATTER_DDMMYYYY),
+                generateRandomTime(9, 17),
+                failureDetectionDate.minusDays(1).format(DATE_FORMATTER_DDMMYYYY) + " " + generateRandomTime(0, 23),
+                "ЗСЖД",
+                getCellStringValue(sourceRow.getCell(7)),
+                concatAndCopyRowsData(sourceRow),
+                getCellStringValue(sourceRow.getCell(17)),
+                "Демянчук В.М., диспетчер",
+                restorationDate.format(DATE_FORMATTER_DDMMYYYY),
+                generateRandomTime(9, 17),
+                getCellStringValue(sourceRow.getCell(18)),
+                getCellStringValue(sourceRow.getCell(20)) + ", инженер ООО \"УК СТС\""
+        };
+    }
 
-        for (int i = 0; i < dates.length; i++) {
+    private static void writeRowData(Row targetRow, String[] data, CellStyle style) {
+        for (int i = 0; i < data.length; i++) {
             Cell targetCell = targetRow.createCell(i);
-            targetCell.setCellValue(dates[i]);
+            targetCell.setCellValue(data[i]);
             targetCell.setCellStyle(style);
         }
     }
 
     private static String concatAndCopyRowsData(Row sourceRow) {
-        int[] columnsToConcat = {6, 7, 9, 13}; // Колонки для объединения
+        int[] columnsToConcat = {6, 7, 9}; // Колонки для объединения
         StringBuilder concatenatedData = new StringBuilder();
 
         for (int columnIndex : columnsToConcat) {
             Cell cell = sourceRow.getCell(columnIndex);
             if (cell != null) {
-                switch (cell.getCellType()) {
-                    case STRING:
-                        if (!concatenatedData.isEmpty()) {
-                            concatenatedData.append("/").append(cell.getStringCellValue());
-                        } else
-                            concatenatedData.append(cell.getStringCellValue());
-                        break;
-                    case NUMERIC:
-                        if (DateUtil.isCellDateFormatted(cell)) {
-                            concatenatedData.append("/").append(cell.getDateCellValue().toString());
-                        } else {
-                            concatenatedData.append("/").append(cell.getNumericCellValue());
-                        }
-                        break;
-                    case BOOLEAN:
-                        concatenatedData.append("/").append(cell.getBooleanCellValue());
-                        break;
-                    default:
-                        break;
+                String cellValue = getCellStringValue(cell);
+                if (cellValue != null && !cellValue.isEmpty()) {
+                    if (concatenatedData.length() > 0) {
+                        concatenatedData.append("/");
+                    }
+                    concatenatedData.append(cellValue);
                 }
-            } else {
-                logger.warn("Ячейка пуста: строка {}, колонка {}", sourceRow.getRowNum(), columnIndex);
             }
         }
-
         concatenatedData.append(" (").append(getCellStringValue(sourceRow.getCell(13))).append(")");
         return concatenatedData.toString().trim();
     }
@@ -196,52 +233,37 @@ public class OFOGTemplateFiller {
         long randomNano = ThreadLocalRandom.current().nextLong(minNano, maxNano);
 
         // Преобразуем случайное число обратно в LocalTime и возвращаем в виде строки
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 //        return LocalTime.ofNanoOfDay(randomNano).truncatedTo(ChronoUnit.MINUTES);
-        return LocalTime.ofNanoOfDay(randomNano).format(formatter);
+        return LocalTime.ofNanoOfDay(randomNano).format(TIME_FORMATTER);
     }
 
-    private static Double getCellNumericValue(Cell cell) {
-        if (cell != null) {
-            switch (cell.getCellType()) {
-                case NUMERIC:
-                    return cell.getNumericCellValue();
-                case STRING:
-                    // Ignore strings that are not valid numbers
-                    try {
-                        return Double.parseDouble(cell.getStringCellValue());
-                    } catch (NumberFormatException e) {
-                        return null; // Invalid number format
-                    }
-                default:
-                    return null;
-            }
-        }
-        return null;
-    }
 
     private static String getCellStringValue(Cell cell) {
-        if (cell != null) {
-            switch (cell.getCellType()) {
-                case STRING:
-                    return cell.getStringCellValue();
-                case NUMERIC:
-                    // Check if it's a date or numeric value
-                    if (DateUtil.isCellDateFormatted(cell)) {
-                        // Format date to dd.MM.yyyy
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-                        return dateFormat.format(cell.getDateCellValue());
-                    } else {
-                        return new DecimalFormat("0").format(cell.getNumericCellValue()); // Convert numeric to string
-                    }
-                case FORMULA:
-                    return String.valueOf(cell.getCellFormula());
-                default:
-                    return null;
-            }
+        if (cell == null) {
+            return "";
         }
-        return null;
+
+        switch (cell.getCellType()) {
+            case STRING:
+                return cell.getStringCellValue();
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+                    return dateFormat.format(cell.getDateCellValue());
+                } else {
+                    return new DecimalFormat("0").format(cell.getNumericCellValue());
+                }
+            case FORMULA:
+                try {
+                    return String.valueOf(cell.getNumericCellValue());
+                } catch (IllegalStateException e) {
+                    return cell.getStringCellValue();
+                }
+            default:
+                return "";
+        }
     }
+
 
     private static CellStyle createDateCellStyle(Workbook resultWorkbook) {
         CellStyle dateCellStyle = resultWorkbook.createCellStyle();
@@ -264,7 +286,7 @@ public class OFOGTemplateFiller {
     public static LocalDate subtractRandomDays(LocalDate date) {
         // Генерируем случайное число: 2 или 3
 //        int daysToSubtract = ThreadLocalRandom.current().nextInt(2, 4); // 4 не включается, т.е. диапазон [2, 3]
-        int daysToSubtract = new Random().nextInt(2, 3);
+        int daysToSubtract = new Random().nextInt(2, 4);
         // Отнимаем случайное количество дней
         return date.minusDays(daysToSubtract);
     }
