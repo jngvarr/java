4f675da9bc15d9bb0a8e59bb254db637c5057d18
jngvarr <jngvarr@gmail.com>
2024-12-09 16:46:59 +0700

    09.12.24

--- TGBot/src/main/java/ru/jngvarr/TGBot/TgBotApplication.java
+++ TGBot/src/main/java/ru/jngvarr/TGBot/TgBotApplication.java
@@ -8,9 +8,15 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import ru.jngvarr.TGBot.config.BotConfig;
 
 @SpringBootApplication
-public class TgBotApplication {
 
+public class TgBotApplication {
+	@PostConstruct
+	public void logEnvironmentVariables() {
+		System.out.println("TELEGRAM_BOT_TOKEN: " + System.getenv("TELEGRAM_BOT_TOKEN"));
+		System.out.println("TELEGRAM_BOT_OWNER: " + System.getenv("TELEGRAM_BOT_OWNER"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TgBotApplication.class, args);
+
 	}
 }
